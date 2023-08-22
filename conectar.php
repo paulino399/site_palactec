f65ac95d8894ca15c2c3a62ec6201b8cc104b1ca
João Paulino <joaopaulino399@gmail.com>
2023-08-23 00:23:07 +0200

    update conectar.php

--- conectar.php
+++ conectar.php
@@ -1,10 +1,22 @@
 <?php
 
 $host = 'localhost';
-$usuario = 'root'
+$usuario = 'root';
+$senha = '';
+$banco = 'site_palancatec';
 
 
 
+$conexao = new MYSQLI($host, $usuario, $senha, $banco);
 
 
+if($conexao -> connect_errno){
+
+    echo = "erro de conexão com o banco de dados";
+
+}
+
+else{
+    echo = "Conexão efectuada com sucesso!";
+}
 ?>
