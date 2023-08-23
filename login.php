f0b7631304d1cdb83865d48d1152661c67e4b024
João Paulino <joaopaulino399@gmail.com>
2023-08-23 04:40:35 +0200

    update login.php

--- login.php
+++ login.php
@@ -8,6 +8,23 @@ if(isset($_POST['submit'])  && !empty($_POST['email'])   && !empty($_POST['senha
     //Conectar o banco de dados
 
     include_once('conectar.php');
+
+//Local de armazenamento
+    $email = $_POST['email'];
+    $email = $_POST['senha'];
+
+    //Verificar a tabela do  banco de dados
+
+    $sql ="SELECT * FROM  cadastro_cliente WHERE email_cliete = '$email' and senha_cliente = '$senha'";
+
+    $verificar = $conexao -> query($sql);
+
+    // Verificar se o registro é valido 
+
+    if(mysql_num_rows ($verificar)<1)
+    {
+        
+    }
 }
 else{
 //O campo esta vazio
