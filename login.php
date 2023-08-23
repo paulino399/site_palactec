72b306ea4bf5ee82e3d9953fc5c475fff82a8cdf
João Paulino <joaopaulino399@gmail.com>
2023-08-23 04:27:58 +0200

    update entrar.css and login.php

--- login.php
+++ login.php
@@ -2,8 +2,18 @@
 
 // Verificar se houve uma acção da pagina entrar.php //
 
-if(isset($_POST['submit'])  && !empty($_POST['email']))
+if(isset($_POST['submit'])  && !empty($_POST['email'])   && !empty($_POST['senha']))
+{
+    //não esta vazio
+    //Conectar o banco de dados
 
+    include_once('conectar.php');
+}
+else{
+//O campo esta vazio
 
+header('Location: entrar.php');
+
+}
 
 ?>
