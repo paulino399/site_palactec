8f0163b8ac003620293b00747e1044b47f82396d
João Paulino <joaopaulino399@gmail.com>
2023-08-25 11:51:14 +0200

    feat: create email.css and email.php

---
+++ email.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PalancaTec</title>
+    <link rel="stylesheet" href="./css/email.css">
+</head>
+<body>
+    <div class="m-conteudo" id="modal-sucesso">
+
+     <div class="modal">
+
+     <img src="img/palanca2.png" alt="logopalancatec">
+     <h2>Obrigado pela mensagem</h2>
+     <p>Nossa equipe ja recebeu o seu <span>E-mail</span> e estamos tomando as devidas providências</p>
+    
+     <p>Entraremos em <span>CONTATO</span> em breve</p>
+
+     <button onclick="sair()" class="fechar">FECHAR</button>
+    </div>
+
+    </div>
+    
+</body>
+</html>
